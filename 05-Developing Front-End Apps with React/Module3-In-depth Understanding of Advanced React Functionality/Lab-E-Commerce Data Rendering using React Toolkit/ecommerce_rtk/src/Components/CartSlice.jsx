b2b1b3202079242d98641e15a4f35ebec09e0563
牛嘉桢 <niujiazhen26@163.com>
2025-02-17 18:22:04 +0800

    Module3 Partly

---
+++ 05-Developing Front-End Apps with React/Module3-In-depth Understanding of Advanced React Functionality/Lab-E-Commerce Data Rendering using React Toolkit/ecommerce_rtk/src/Components/CartSlice.jsx
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+    cartItems:[],
+};
+const CartSlice = createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addItemToCart(state,action){
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+            if(existingItem){
+                existingItem.quantity++;
+            }
+            else{
+                state.cartItems.push({...action.payload,quantity: 1});
+            }
+        },
+
+        removeItemFromCart(state,action){
+            state.cartItems=state.cartItems.filter(item => item.id !== action.payload.id);
+        },
+
+        clearCart(state){
+            state.cartItems=[];
+        },
+
+        increaseItemQuantity(state,action){
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload.id);
+            if(itemToIncrease){
+                itemToIncrease.quantity++;
+            }
+        },
+
+        decreaseItemQuantity(state,action){
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload.id);
+            if(itemToDecrease && itemToDecrease.quantity > 1){
+                itemToDecrease.quantity--;
+            }
+        }
+    }
+});
+
+export const{
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity
+} = CartSlice.actions;
+export default CartSlice.reducer;
+
+
