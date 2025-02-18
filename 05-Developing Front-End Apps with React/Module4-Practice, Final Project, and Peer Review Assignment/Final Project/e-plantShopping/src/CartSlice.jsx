ee4fcf8d64ac9ca0d407562b2777ec7a063c7ccc
niujiazhen <niujiazhen26@163.com>
2025-02-18 22:43:55 +0800

    Module4 Final Project Partly

---
+++ 05-Developing Front-End Apps with React/Module4-Practice, Final Project, and Peer Review Assignment/Final Project/e-plantShopping/src/CartSlice.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const {name, image, cost} = action.payload
+      const existingItem = state.items.find(item => item.name === name)
+    },
+    removeItem: (state, action) => {
+    },
+    updateQuantity: (state, action) => {
+
+    
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
